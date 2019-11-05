1704943b57b713d0db27cfc5733741ef28c6f0cd
rgarnacho <56841950+rgarnacho@users.noreply.github.com>
2019-11-05 18:41:34 +0100

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,4 +1,4 @@
-job('NodeJS example') {
+job('RG: NodeJS example') {
     scm {
         git('http://github.com/rgarnacho/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
